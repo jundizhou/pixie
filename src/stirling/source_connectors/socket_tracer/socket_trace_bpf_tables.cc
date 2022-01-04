3a44f034ad24d66ee8f89e407d6eb9f30182f374
qianlu.kk <qianlu.lk@alibaba-inc.com>
2022-01-04 21:49:56 +0800

    rm unused-variable

--- src/stirling/source_connectors/socket_tracer/socket_trace_bpf_tables.cc
+++ src/stirling/source_connectors/socket_tracer/socket_trace_bpf_tables.cc
@@ -47,7 +47,7 @@ ConnInfoMapManager::ConnInfoMapManager(bpf_tools::BCCWrapper* bcc)
       conn_disabled_map_(bcc->GetHashTable<uint64_t, uint64_t>("conn_disabled_map")) {
   // Use address instead of symbol to specify this probe,
   // so that even if debug symbols are stripped, the uprobe can still attach.
-  uint64_t symbol_addr = reinterpret_cast<uint64_t>(&ConnInfoMapCleanupTrigger);
+  // uint64_t symbol_addr = reinterpret_cast<uint64_t>(&ConnInfoMapCleanupTrigger);
 
   std::filesystem::path self_path = GetSelfPath().ValueOrDie();
 
